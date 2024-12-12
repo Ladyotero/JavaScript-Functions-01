0573b1a3bbef1cf7b812d59f22a5e945e524244f
Rebecca Otero <iotero07@gmail.com>
2024-12-12 17:47:44 -0500

    Day 1 JS Lab 1

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+console.log("Cheers")
+
+/* the title of the song: Life's adventure
+
+Underneath the golden sun, Waves are crashing, everyone, 
+Tacos in the sun, life has just begun, On the beach, we're having fun, 
+Pickleball at dawn, travel 'til we're gone, This is how we run, 
+this is life we love! Journey far and roam the skies, 
+Life's adventure never dies, Tacos in the sun, 
+life has just begun, On the beach, we're having fun, Pickleball at dawn, 
+travel 'til we're gone, This is how we run, this is life we love!
+*/
+
+function refrain(){
+
+    //declare, contains the repeated lyrics as seperated console.log statments.
+   console.log("Tacos in the sun, life has just begun");
+   console.log("On the beach, we're having fun");
+   console.log("Pickleball at dawn, travel 'til we're gone");
+   console.log("This is how we run, this is life we love!");
+
+}
+
+function song(){
+// call the refrain function wherever the repeated lines appear.
+   
+}
+
